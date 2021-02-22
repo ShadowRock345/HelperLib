3683a7a3f657176f28a617ca82b399cf00079cc4
Jonas Babernits <jonba297@gmail.com>
2021-02-22 08:59:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# HelperLib" 
+"# HelperLib" 
---
+++ helperlib/src/main/java/com/example/helperlib/HelperLib.java
@@ -0,0 +1,16 @@
+package com.example.helperlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.net.ContentHandler;
+
+public class HelperLib {
+
+    public void shortToast(Context context, String text){
+        int length = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, length);
+        toast.show();
+    }
+
+}
