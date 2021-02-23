7947104def41b9bb0f334f5e55dd3de00a0c4318
ShadowRock345 <79307698+ShadowRock345@users.noreply.github.com>
2021-02-23 11:45:45 +0100

    Update HelperLib.java

--- helperlib/src/main/java/com/example/helperlib/HelperLib.java
+++ helperlib/src/main/java/com/example/helperlib/HelperLib.java
@@ -12,5 +12,17 @@ public class HelperLib {
         Toast toast = Toast.makeText(context, text, length);
         toast.show();
     }
+    
+    public static void longToast(Context context, String text){
+        int length = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, length);
+        toast.show();
+    }
+
+    public static int addieren(Integer integerNummer1, Integer integerNummer2){
+        int ergebniss = integerNummer1 + integerNummer2;
+        return ergebniss;
+    }
+
 
 }
